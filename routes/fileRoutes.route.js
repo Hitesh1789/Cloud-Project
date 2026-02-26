398c46bbec90d50a2e0e2b5c759f696ef1973dca
Hitesh1789 <hiteshmehandiratta84@gmail.com>
2026-02-26 13:20:49 +0530

    Fixed fileRoutes

--- routes/fileRoutes.route.js
+++ routes/fileRoutes.route.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import { uploadFile ,deleteFile,showVersions,downloadFile} from "../controllers/file.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
+
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this fil
 
 router.post("/uploadFile", upload.single("file"), uploadFile);
 router.delete("/deleteFile/:fileId",deleteFile);
