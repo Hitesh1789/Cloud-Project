45faf3d848d188c9f37a76149ae18d0009dbd506
Hitesh1789 <hiteshmehandiratta84@gmail.com>
2026-02-26 23:08:26 +0530

    Try to fixauth error.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -83,7 +83,8 @@ const loginUser = async (req, res) => {
 
         const options = {
             httpOnly: true,
-            secure: false
+            secure: false,
+            sameSite: "lax"  
         }
 
         return res.status(200)
