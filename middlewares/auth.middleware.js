1bb98d7324a2e6ff5581ded925e1e089503433f7
Hitesh1789 <hiteshmehandiratta84@gmail.com>
2026-02-26 13:20:28 +0530

    Fixed auth middleware and allow file access to only verififed users.

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -3,21 +3,25 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
  
-export const verifyJWT = asyncHandler(async (req,res,next)=>{
-    const accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","");
-
-    if(!accessToken){
-        throw new ApiError(401,"Unauthorised request!")
+export const verifyJWT = async (req,res,next)=>{
+    try {
+        const accessToken = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","");
+    
+        if(!accessToken){
+            throw new ApiError(401,"Unauthorised request!")
+        }
+    
+        const decodedToken = jwt.verify(accessToken,process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+        if(!user){
+            throw new ApiError(401,"Invalid access token!")
+        }
+    
+        req.user = user;
+        next()
+    } catch (error) {
+        console.log(error)
     }
-
-    const decodedToken = jwt.verify(accessToken,process.env.ACCESS_TOKEN_SECRET)
-
-    const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
-
-    if(!user){
-        throw new ApiError(401,"Invalid access token!")
-    }
-
-    req.user = user;
-    next()
-})
+}
